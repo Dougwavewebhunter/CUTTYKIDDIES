b5f6b9f45dfaf24658ce9a4fbcb262f3b49fee07
Dougwavewebhunter <douglaswebhunter@gmail.com>
2025-08-17 03:25:15 +0200

    Fix missing Heart icon imports

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Mail, Phone, MapPin, Facebook, Instagram, Twitter, Shield, Lock, Award } from 'lucide-react';
+import { Mail, Phone, MapPin, Facebook, Instagram, Twitter, Shield, Lock, Award, Heart } from 'lucide-react';
 
 const Footer: React.FC = () => {
   return (
